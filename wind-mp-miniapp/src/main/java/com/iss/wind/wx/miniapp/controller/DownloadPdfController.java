cd5b6c53dc042325dd37e01fc760c337c9076003
jiaming <jmhong@isoftstone.com>
2022-04-09 21:20:59 +0800

    PDFExport

--- wind-mp-miniapp/src/main/java/com/iss/wind/wx/miniapp/controller/DownloadPdfController.java
+++ wind-mp-miniapp/src/main/java/com/iss/wind/wx/miniapp/controller/DownloadPdfController.java
@@ -3,7 +3,6 @@ package com.iss.wind.wx.miniapp.controller;
 import com.iss.wind.client.dto.shipmenttracking.ShipmentTrackingResp;
 import com.iss.wind.common.util.log.WebLog;
 import com.iss.wind.serevice.export.TrackingExportService;
-import com.iss.wind.serevice.util.PDFUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -16,14 +15,11 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
-import java.net.URLEncoder;
 import java.util.*;
 
 /**
@@ -41,37 +37,31 @@ public class DownloadPdfController {
     /**
      * 下载PDF
      *
-     * @param response
+     *
      */
     @PostMapping("/downloadPdf")
     @ApiOperation(value = "pdf模板导出", notes = "pdf模板导出")
     @ApiResponses(value = {@ApiResponse(code = 500, message = "failedOrTimeOut")})
     //@WebLog(description = "downloadPdf")
-    public void download(@RequestBody ShipmentTrackingResp trackingResp, HttpServletResponse response) {
-        try (BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream())) {
-            //ObjectMapper mapper = new ObjectMapper();
-            //ShipmentTrackingResp trackingResp = mapper.readValue(tracking, ShipmentTrackingResp.class);
+    public String download(@RequestBody ShipmentTrackingResp trackingResp) {
+        try {
             //String Path1 = "D:\\Wind\\wind-mp\\wind-mp-miniapp\\src\\main\\resources\\pdfTemplate\\tracking.jrxml";
-            //String Path2 = "D:\\Wind\\wind-mp\\wind-mp-miniapp\\src\\main\\resources\\pdfTemplate\\tracking.jasper";
-            //JasperCompileManager.compileReportToFile(Path1, Path2);
             log.info("\n接收到来自前台的数据:{}", trackingResp);
             ClassPathResource resource = new ClassPathResource("pdfTemplate/tracking.jasper");
+            //JasperCompileManager.compileReportToFile(Path1, resource.getFile().getPath());
             FileInputStream fis = new FileInputStream(resource.getFile());
+            String path = new ClassPathResource("static").getFile().getPath();
+            String filePath = path+File.separator+UUID.randomUUID().toString().replace("-","")+"-Tracking.pdf";
             //调用方法以获取正确的数据格式;
             HashMap<String, Object> map = exportService.getColumnHeader(trackingResp);
             ArrayList<Map<String, Object>> list = exportService.getDetails(trackingResp);
             //对模板文件进行填充形成新文件,写入到输出流;
             JasperPrint jasperPrint = JasperFillManager.fillReport(fis, map, new JRBeanCollectionDataSource(list));
-            JasperExportManager.exportReportToPdfStream(jasperPrint, out);
-
-            response.setContentType(MediaType.APPLICATION_PDF_VALUE);
-            response.setCharacterEncoding(PDFUtils.ENCODING);
-            String fileName = URLEncoder.encode("shipment-tracking.pdf", PDFUtils.ENCODING);
-            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
-            out.flush();
-            out.close();
+            JasperExportManager.exportReportToPdfFile(jasperPrint,filePath);
+            log.info("\nPdf文件地址:{}",filePath);
+            return filePath;
         } catch (Exception e) {
-            log.error("异常：", e);
+            log.error("异常:", e);
             throw new RuntimeException("下载PDF方法异常");
         }
     }
