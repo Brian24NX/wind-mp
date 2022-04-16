ba0c2c7fdc6c0e3f16e13f3b05863978b996b081
yutuy <2547848263@qq.com>
2022-04-16 13:15:24 +0800

    commit update swagger

--- wind-mp-admin/src/main/java/com/iss/wind/Application.java
+++ wind-mp-admin/src/main/java/com/iss/wind/Application.java
@@ -2,18 +2,16 @@ package com.iss.wind;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Hanson
  * @date 2021/11/18  17:49
  */
-//@SpringBootApplication
-@EnableAutoConfiguration(exclude = { FreeMarkerAutoConfiguration.class })
+@SpringBootApplication
 @MapperScan(basePackages = "com.iss.wind.*")
+@ComponentScan(basePackages = "com.iss.wind.*")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- wind-mp-offiaccount/src/main/java/com/iss/wind/wx/offiaccount/OffiAccountApplication.java
+++ wind-mp-offiaccount/src/main/java/com/iss/wind/wx/offiaccount/OffiAccountApplication.java
@@ -2,17 +2,14 @@ package com.iss.wind.wx.offiaccount;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Hanson
  * @date 2021/11/18  17:49
  */
-//@SpringBootApplication
-@EnableAutoConfiguration(exclude = { FreeMarkerAutoConfiguration.class })
+@SpringBootApplication
 @MapperScan(basePackages = "com.iss.wind.*")
 @ComponentScan(basePackages = "com.iss.wind.*")
 public class OffiAccountApplication {
